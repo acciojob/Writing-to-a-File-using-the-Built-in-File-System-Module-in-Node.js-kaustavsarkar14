8edf10650151bc3a6fa9091bcfa005e73ca8d2ab
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2024-01-21 11:15:46 +0530

    test

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ const fs = require('fs');
 
 // TODO: Write the message "Hello, World!" to the file "output.txt"
 
+fs.writeFile('output.txt', "Hello, World!", err => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("file written successfully")
+    }
+  });
